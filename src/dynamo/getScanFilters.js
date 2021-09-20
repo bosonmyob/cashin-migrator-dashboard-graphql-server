f81b0e915b71ae1b56da95845e3a1ef2157b9417
Boson Huang <boson.huang@myob.com>
2021-09-20 22:52:40 +1000

    updated scan filters to returns an array rather than an object

--- src/dynamo/getScanFilters.js
+++ src/dynamo/getScanFilters.js
@@ -4,21 +4,19 @@
  * @param {number} params.startTimestamp
  * @param {number} params.endTimestamp
  */
-const getScanFilters = ({ startTimestamp, endTimestamp }) => ({
-  fields: [
-    {
-      key: 'timestamp',
-      expression: 'startTimestamp',
-      value: startTimestamp,
-      ops: '>='
-    },
-    {
-      key: 'timestamp',
-      expression: 'endTimestamp',
-      value: endTimestamp,
-      ops: '<='
-    }
-  ]
-});
+const getScanFilters = ({ startTimestamp, endTimestamp }) => [
+  {
+    key: 'timestamp',
+    expression: 'startTimestamp',
+    value: startTimestamp,
+    ops: '>='
+  },
+  {
+    key: 'timestamp',
+    expression: 'endTimestamp',
+    value: endTimestamp,
+    ops: '<='
+  }
+];
 
 module.exports = getScanFilters;
